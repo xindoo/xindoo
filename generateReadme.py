241a8827196e2d8669dc153df3675d7afb252cc8
xindoo <xindoo@qq.com>
2020-07-24 21:06:27 +0800

    add update readme action

---
+++ generateReadme.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import urllib3
+from lxml import etree
+import html
+import re
+ 
+http = urllib3.PoolManager(num_pools=5, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'})
+ 
+resp = http.request('GET', 'https://xindoo.blog.csdn.net/')
+
+reshtml = etree.HTML(resp.data.decode("utf-8"))
+html_data = reshtml.xpath(".//div[@class='article-item-box csdn-tracking-statistics']/h4") 
+f = open('README.md', 'w+')
+f.write("## 我的最新博客\n")
+
+cnt = 0
+for i in html_data: 
+	if cnt >= 10:
+		break
+	title = i.xpath('./a/text()')[1].strip()
+	url = i.xpath('./a/@href')[0] 
+	txt = '- [%s](%s)\n' % (title, url)
+	f.write(txt)
+	cnt = cnt + 1
+f.close
+   
