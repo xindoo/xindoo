01bdc0ea85ec5488a1d823dbd96189f3ffc89e23
xindoo <xindoo@qq.com>
2020-07-25 14:04:26 +0800

    fds

--- generateReadme.py
+++ generateReadme.py
@@ -20,12 +20,11 @@ def addIntro(f):
 
 def addProjectInfo(f):
 	txt ='''
-- [eng-practices-cn](https://github.com/xindoo/eng-practices-cn)谷歌工程实践中文版  
-- [regex](https://github.com/xindoo/regex)Java实现的正则引擎表达式  
-- [redis](https://github.com/xindoo/redis) Redis中文注解版
+- [eng-practices-cn](https://github.com/xindoo/eng-practices-cn)谷歌工程实践中文版    
+- [regex](https://github.com/xindoo/regex)Java实现的正则引擎表达式    
+- [redis](https://github.com/xindoo/redis) Redis中文注解版  
 ### [查看更多](https://github.com/xindoo/)   
 
-	
 	''' 
 	f.write(txt) 
 
@@ -38,7 +37,7 @@ def addBlogInfo(f):
 	f.write("## 我的博客\n")
 	cnt = 0
 	for i in html_data: 
-		if cnt >= 8:
+		if cnt >= 5:
 			break
 		title = i.xpath('./a/text()')[1].strip()
 		url = i.xpath('./a/@href')[0] 
@@ -53,10 +52,10 @@ f.write('<table><tr>\n')
 f.write('<td valign="top" width="50%">\n')
 
 addProjectInfo(f)
-f.write('</td>')
+f.write('</td>\n')
 f.write('<td valign="top" width="50%">\n')
 addBlogInfo(f)
-f.write('</td>')
+f.write('</td>\n')
 f.write('</tr></table>\n')
 f.close 
 
