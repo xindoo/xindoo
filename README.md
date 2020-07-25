6a40b771ed7e932032b85121f30985bdec70b5dd
xindoo <xindoo@qq.com>
2020-07-25 09:58:29 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## 我的最新博客
+## 我的博客
 - [JDK11现存性能bug(JDK-8221393)深度解析](https://xindoo.blog.csdn.net/article/details/107291487)
 - [生日悖论是啥？我用它省了上百G的内存](https://xindoo.blog.csdn.net/article/details/107120414)
 - [为什么美国程序员工作比中国程序员工作轻松、加班少？](https://xindoo.blog.csdn.net/article/details/107081746)
@@ -9,3 +9,5 @@
 - [面试题精选:循环队列](https://xindoo.blog.csdn.net/article/details/106035350)
 - [从0到1打造正则表达式执行引擎(一)](https://xindoo.blog.csdn.net/article/details/105875239)
 - [面试题精选:求根号2简单？高级算法你肯定不会](https://xindoo.blog.csdn.net/article/details/105456524)
+
+##### [查看更多](https://xindoo.blog.csdn.net/)
--- generateReadme.py
+++ generateReadme.py
@@ -3,24 +3,29 @@ import urllib3
 from lxml import etree
 import html
 import re
- 
-http = urllib3.PoolManager(num_pools=5, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'})
- 
-resp = http.request('GET', 'https://xindoo.blog.csdn.net/')
 
-resp_tree = etree.HTML(resp.data.decode("utf-8"))
-html_data = resp_tree.xpath(".//div[@class='article-item-box csdn-tracking-statistics']/h4") 
+blogUrl = 'https://xindoo.blog.csdn.net/'
+headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'} 
+ 
+
+def addBlogInfo(f):  
+	http = urllib3.PoolManager(num_pools=5, headers = headers)
+	resp = http.request('GET', blogUrl)
+	resp_tree = etree.HTML(resp.data.decode("utf-8"))
+	html_data = resp_tree.xpath(".//div[@class='article-item-box csdn-tracking-statistics']/h4") 
+	f.write("## 我的博客\n")
+	cnt = 0
+	for i in html_data: 
+		if cnt >= 10:
+			break
+		title = i.xpath('./a/text()')[1].strip()
+		url = i.xpath('./a/@href')[0] 
+		item = '- [%s](%s)\n' % (title, url)
+		f.write(item)
+		cnt = cnt + 1
+	f.write('\n##### [查看更多](https://xindoo.blog.csdn.net/)\n')
+
 f = open('README.md', 'w+')
-f.write("## 我的博客\n")
+addBlogInfo(f)
+f.close 
 
-cnt = 0
-for i in html_data: 
-	if cnt >= 10:
-		break
-	title = i.xpath('./a/text()')[1].strip()
-	url = i.xpath('./a/@href')[0] 
-	txt = '- [%s](%s)\n' % (title, url)
-	f.write(txt)
-	cnt = cnt + 1
-f.close
-   
