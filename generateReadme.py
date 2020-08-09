f39582ec03bb7e7f3bce3517e586406aa2f43763
xindoo <xindoo@qq.com>
2020-08-09 18:09:38 +0800

    update

--- generateReadme.py
+++ generateReadme.py
@@ -12,7 +12,7 @@ def addIntro(f):
 	txt = ''' 
 9年技术博主，CSDN认证博客专家，新晋视频up主  
 曾在阿里做过2年运维+1年开发，现为某厂Java后端开发工程师，拥有丰富的 挖坑 踩坑 填坑 背锅经验 🐶   
-专注于Java，对操作系统、网络、编译原理也有涉猎，目前正在写一门简易的脚本语言u-lang     
+专注于Java，对操作系统、网络、编译原理也有涉猎，目前正在写一门简易的脚本语言u-lang	 
 
 
 ''' 
@@ -21,13 +21,13 @@ def addIntro(f):
 def addProjectInfo(f):
 	txt ='''
 ### 开源项目  
-- [eng-practices-cn](https://github.com/xindoo/eng-practices-cn)谷歌工程实践中文版    
-- [regex](https://github.com/xindoo/regex)Java实现的正则引擎表达式    
+- [eng-practices-cn](https://github.com/xindoo/eng-practices-cn)谷歌工程实践中文版	
+- [regex](https://github.com/xindoo/regex)Java实现的正则引擎表达式	
 - [redis](https://github.com/xindoo/redis) Redis中文注解版  
 - [slowjson](https://github.com/xindoo/slowjson) 用antlr实现的json解析器  
 - [leetcode](https://github.com/xindoo/leetcode) 我的Leetcode题解   
    
-[查看更多](https://github.com/xindoo/)     
+[查看更多](https://github.com/xindoo/)	 
 
 	''' 
 	f.write(txt) 
@@ -53,15 +53,14 @@ def addBlogInfo(f):
 
 if __name__=='__main__':
 	f = open('README.md', 'w+')
-    addIntro(f)
-    f.write('<table><tr>\n')
-    f.write('<td valign="top" width="50%">\n')
-
-    addProjectInfo(f)
-    f.write('\n</td>\n')
-    f.write('<td valign="top" width="50%">\n')
-    addBlogInfo(f)
-    f.write('\n</td>\n')
-    f.write('</tr></table>\n')
-    f.close 
+	addIntro(f)
+	f.write('<table><tr>\n')
+	f.write('<td valign="top" width="50%">\n')
+	addProjectInfo(f)
+	f.write('\n</td>\n')
+	f.write('<td valign="top" width="50%">\n')
+	addBlogInfo(f)
+	f.write('\n</td>\n')
+	f.write('</tr></table>\n')
+	f.close 
 
