e29669ff99221caa297433b653ca0e82512a3023
xindoo <xindoo@qq.com>
2023-05-29 22:15:54 +0800

    update blog url

--- generateReadme.py
+++ generateReadme.py
@@ -4,7 +4,7 @@ from lxml import etree
 import html
 import re
 
-blogUrl = 'https://xindoo.blog.csdn.net/?type=blog'
+blogUrl = 'https://blog.csdn.net/xindoo?type=blog'
 
 headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'} 
 
