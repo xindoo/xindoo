ced45d886009486c67b8f26dc62d5ee00fba87aa
xindoo <xindoo@qq.com>
2024-04-30 17:01:37 +0800

    update

--- generateReadme.py
+++ generateReadme.py
@@ -6,7 +6,8 @@ import re
 
 blogUrl = 'https://blog.csdn.net/xindoo?type=blog'
 
-headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'} 
+headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36' \
+  --data-raw '{"coupon":true}'} 
 
 def addIntro(f):
 	txt = '''  
